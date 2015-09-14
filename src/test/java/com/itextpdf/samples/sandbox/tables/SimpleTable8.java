d2a10d1bf5dc18be1db0e3f769aae256ec5d4fe5
Pavel Morozov <pavel.morozov@duallab.com>
2015-09-14 11:24:23 +0300

    Creating FormXObjects by pages
    DEVSIX-238

--- src/test/java/com/itextpdf/samples/sandbox/tables/SimpleTable8.java
+++ src/test/java/com/itextpdf/samples/sandbox/tables/SimpleTable8.java
@@ -1,10 +1,13 @@
 package com.itextpdf.samples.sandbox.tables;
 
 import com.itextpdf.core.pdf.PdfDocument;
+import com.itextpdf.core.pdf.PdfReader;
 import com.itextpdf.core.pdf.PdfWriter;
+import com.itextpdf.core.pdf.xobject.PdfFormXObject;
 import com.itextpdf.core.testutils.annotations.type.SampleTest;
 import com.itextpdf.model.Document;
 import com.itextpdf.model.element.Cell;
+import com.itextpdf.model.element.Image;
 import com.itextpdf.model.element.Paragraph;
 import com.itextpdf.model.element.Table;
 import com.itextpdf.samples.GenericTest;
@@ -18,6 +21,7 @@ import java.io.FileOutputStream;
 @Category(SampleTest.class)
 public class SimpleTable8 extends GenericTest {
     public static final String DEST = "./target/test/resources/sandbox/tables/simple_table8.pdf";
+    public static final String SRC = "./src/test/resources/sandbox/tables/";
 
     public static void main(String[] args) throws Exception {
         File file = new File(DEST);
@@ -33,21 +37,21 @@ public class SimpleTable8 extends GenericTest {
         Document doc = new Document(pdfDoc);
 
         Table table = new Table(3);
-        //PdfReader reader = new PdfReader("./target/test/resources/sandbox/tables/header.pdf");
-        // TODO Implement functionality to do things below
-        // PdfImportedPage header = writer.getImportedPage(reader, 1);
-        // Cell cell = new Cell(1,3).add(Image.getInstance(header));
-        //table.addCell(cell);
+        PdfReader reader = new PdfReader(SRC + "header.pdf");
+        PdfDocument srcDoc = new PdfDocument(reader);
+        PdfFormXObject header = srcDoc.getFirstPage().copyAsFormXObject(pdfDoc);
+        Cell cell = new Cell(1,3).add(new Image(header).setAutoScale(true));
+        table.addCell(cell);
         for (int row = 1; row <= 50; row++) {
             for (int column = 1; column <= 3; column++) {
                 table.addCell(new Cell().add(new Paragraph(String.format("row %s, column %s", row, column))));
             }
         }
-        //reader = new PdfReader("./target/test/resources/sandbox/tables/footer.pdf");
-        // TODO Implement functionality to do things below
-        //PdfImportedPage footer = writer.getImportedPage(reader, 1);
-        //cell = new Cell(1,3).add(Image.getInstance(footer));
-        //table.addCell(cell);
+        reader = new PdfReader(SRC + "footer.pdf");
+        srcDoc = new PdfDocument(reader);
+        PdfFormXObject footer = srcDoc.getFirstPage().copyAsFormXObject(pdfDoc);
+        cell = new Cell(1,3).add(new Image(footer).setAutoScale(true));
+        table.addCell(cell);
         doc.add(table);
 
         doc.close();
