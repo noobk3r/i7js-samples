eeeba4edbb4fd52f9cba86bd42c27203d5ed5f42
Alexey Subach <alexey.subach@duallab.com>
2015-09-22 14:32:49 +0300

    Fix CompareTool bug when comparing font subsets. Migrate several tests to compare by content
    DEVSIX-292

--- src/test/java/com/itextpdf/samples/sandbox/acroforms/CreateJapaneseButton.java
+++ src/test/java/com/itextpdf/samples/sandbox/acroforms/CreateJapaneseButton.java
@@ -14,13 +14,11 @@ import com.itextpdf.forms.PdfAcroForm;
 import com.itextpdf.forms.fields.PdfButtonFormField;
 import com.itextpdf.forms.fields.PdfFormField;
 import com.itextpdf.samples.GenericTest;
-import org.junit.Ignore;
 import org.junit.experimental.categories.Category;
 
 import java.io.File;
 import java.io.FileOutputStream;
 
-@Ignore
 @Category(SampleTest.class)
 public class CreateJapaneseButton extends GenericTest {
     public static final String JAPANESE = "\u3042\u304d\u3089";
