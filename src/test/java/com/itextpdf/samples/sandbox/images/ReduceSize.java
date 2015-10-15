672963333e5d0b86ea773de186c339b300ad8663
Dmitry Trusevich <dmitry.trusevich@duallab.com>
2015-10-15 15:33:08 +0300

    fix failure build

--- src/test/java/com/itextpdf/samples/sandbox/images/ReduceSize.java
+++ src/test/java/com/itextpdf/samples/sandbox/images/ReduceSize.java
@@ -49,7 +49,7 @@ public class ReduceSize extends GenericTest {
         PdfDocument pdfDoc = new PdfDocument(new PdfReader(new FileInputStream(SRC)), writer);
         PdfObject object;
         PdfStream stream;
-        for (PdfIndirectReference indRef : pdfDoc.listIndirectReferences()) {
+        for (PdfIndirectReference indRef : pdfDoc.getIndirectReferences()) {
             object = indRef.getRefersTo();
             if (object == null || !object.isStream())
                 continue;
