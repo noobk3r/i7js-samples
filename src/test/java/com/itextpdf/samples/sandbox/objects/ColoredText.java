cdf7680b6dbcd11824c41ee76fa2bedfa36686cf
Alexey Subach <alexey.subach@duallab.com>
2015-11-04 11:31:00 +0300

    More work on samples

--- src/test/java/com/itextpdf/samples/sandbox/objects/ColoredText.java
+++ src/test/java/com/itextpdf/samples/sandbox/objects/ColoredText.java
@@ -5,13 +5,15 @@
 package com.itextpdf.samples.sandbox.objects;
 
 import com.itextpdf.basics.font.FontConstants;
+import com.itextpdf.basics.geom.Rectangle;
+import com.itextpdf.canvas.PdfCanvas;
 import com.itextpdf.canvas.color.Color;
 import com.itextpdf.core.font.PdfFont;
 import com.itextpdf.core.pdf.PdfDocument;
 import com.itextpdf.core.pdf.PdfWriter;
 import com.itextpdf.core.testutils.annotations.type.SampleTest;
+import com.itextpdf.model.Canvas;
 import com.itextpdf.model.Document;
-import com.itextpdf.model.Property;
 import com.itextpdf.model.element.Paragraph;
 import com.itextpdf.model.element.Text;
 import com.itextpdf.samples.GenericTest;
@@ -36,31 +38,28 @@ public class ColoredText extends GenericTest {
         PdfDocument pdfDoc = new PdfDocument(new PdfWriter(new FileOutputStream(dest)));
         Document doc = new Document(pdfDoc);
 
-        // TODO Implement setNormal() on PdfFont objects
         Text redText = new Text("This text is red. ")
                 .setFontColor(Color.RED)
                 .setFontSize(12)
                 .setFont(PdfFont.createStandardFont(pdfDoc, FontConstants.HELVETICA));
-        Text blueText = new Text("This text is blue and bold")
+        Text blueText = new Text("This text is blue and bold. ")
                 .setFontColor(Color.BLUE)
                 .setFontSize(12)
                 .setFont(PdfFont.createStandardFont(pdfDoc, FontConstants.HELVETICA_BOLD));
         Text greenText = new Text("This text is green and italic. ")
                 .setFontColor(Color.GREEN)
                 .setFontSize(12)
-                .setFont(PdfFont.createStandardFont(pdfDoc, FontConstants.HELVETICA))
-                .setItalic();
-        Paragraph p1 = new Paragraph(redText);
+                .setFont(PdfFont.createStandardFont(pdfDoc, FontConstants.HELVETICA_OBLIQUE));
+
+        Paragraph p1 = new Paragraph(redText).setMargin(0);
         doc.add(p1);
-        Paragraph p2 = new Paragraph();
+        Paragraph p2 = new Paragraph().setMargin(0);
         p2.add(blueText);
         p2.add(greenText);
         doc.add(p2);
-        // TODO There is no ColumnText in itex6, but Document.add isn't quite the same
-        doc.showTextAligned(p1.setWidth(144 - 36).setHeight(760 - 600), 36, 480, 1,
-                Property.HorizontalAlignment.LEFT, Property.VerticalAlignment.BOTTOM, 0);
-        doc.showTextAligned(p2.setWidth(144 - 36).setHeight(760 - 600), 36, 460, 1,
-                Property.HorizontalAlignment.LEFT, Property.VerticalAlignment.BOTTOM, 0);
+
+        new Canvas(new PdfCanvas(pdfDoc.getLastPage()), pdfDoc, new Rectangle(36, 600, 108, 160)).
+            add(p1).add(p2);
 
         doc.close();
     }
