212abf4d206dd441f785da86fe7eca6afb18d882
Alexey Subach <alexey.subach@duallab.com>
2015-11-05 14:40:26 +0300

    Fix Color import

--- src/test/java/com/itextpdf/samples/sandbox/objects/ColoredText.java
+++ src/test/java/com/itextpdf/samples/sandbox/objects/ColoredText.java
@@ -7,7 +7,6 @@ package com.itextpdf.samples.sandbox.objects;
 import com.itextpdf.basics.font.FontConstants;
 import com.itextpdf.basics.geom.Rectangle;
 import com.itextpdf.canvas.PdfCanvas;
-import com.itextpdf.canvas.color.Color;
 import com.itextpdf.core.color.Color;
 import com.itextpdf.core.font.PdfFont;
 import com.itextpdf.core.pdf.PdfDocument;
