cc0ee6c5dc96ff279fab844bb3ebbd30ca419216
Dmitry Trusevich <dmitry.trusevich@duallab.com>
2015-11-13 14:24:03 +0300

    Implement filling and removing XFA forms
    (DEVSIX-324)

--- src/test/java/com/itextpdf/samples/sandbox/acroforms/FillXFA2.java
+++ src/test/java/com/itextpdf/samples/sandbox/acroforms/FillXFA2.java
@@ -7,13 +7,11 @@ import com.itextpdf.core.testutils.annotations.type.SampleTest;
 import com.itextpdf.forms.PdfAcroForm;
 import com.itextpdf.forms.xfa.XfaForm;
 import com.itextpdf.samples.GenericTest;
-import com.itextpdf.text.pdf.AcroFields;
-import com.itextpdf.text.pdf.PdfStamper;
 import org.junit.experimental.categories.Category;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
+
 
 @Category(SampleTest.class)
 public class FillXFA2 extends GenericTest {
@@ -21,6 +19,8 @@ public class FillXFA2 extends GenericTest {
     public static final String SRC = "./src/test/resources/sandbox/acroforms/xfa_form_poland.pdf";
     public static final String XML = "./src/test/resources/sandbox/acroforms/xfa_form_poland.xml";
     public static final String DEST = "./target/test/resources/sandbox/acroforms/xfa_form_poland_filled.pdf";
+
+
 
     public static void main(String[] args) throws Exception {
         File file = new File(DEST);
