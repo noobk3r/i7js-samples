7b5b9e09a8093ea2fc763da7c5fbcdc553b27d9c
Vladimir Osipchuk <vladimir.osipchuk@duallab.com>
2016-01-27 17:28:39 +0300

    Add handy method to set page rotation. Correct some samples

--- src/test/java/com/itextpdf/samples/sandbox/objects/CenterColumnVertically.java
+++ src/test/java/com/itextpdf/samples/sandbox/objects/CenterColumnVertically.java
@@ -8,23 +8,22 @@
 package com.itextpdf.samples.sandbox.objects;
 
 import com.itextpdf.basics.geom.Rectangle;
-import com.itextpdf.core.pdf.canvas.PdfCanvas;
 import com.itextpdf.core.color.Color;
 import com.itextpdf.core.pdf.PdfDocument;
 import com.itextpdf.core.pdf.PdfWriter;
-import com.itextpdf.test.annotations.type.SampleTest;
+import com.itextpdf.core.pdf.canvas.PdfCanvas;
+import com.itextpdf.model.Canvas;
 import com.itextpdf.model.Document;
 import com.itextpdf.model.element.Paragraph;
 import com.itextpdf.samples.GenericTest;
+import com.itextpdf.test.annotations.type.SampleTest;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import org.junit.Ignore;
 import org.junit.experimental.categories.Category;
 
-@Ignore
 @Category(SampleTest.class)
 public class CenterColumnVertically extends GenericTest {
     public static final String DEST = "./target/test/resources/sandbox/objects/center_column_vertically.pdf";
@@ -38,25 +37,16 @@ public class CenterColumnVertically extends GenericTest {
     public void manipulatePdf(String dest) throws IOException {
         PdfDocument pdfDoc = new PdfDocument(new PdfWriter(new FileOutputStream(dest)));
         Document doc = new Document(pdfDoc);
-
         float llx = 50;
         float lly = 650;
         float urx = 400;
         float ury = 800;
-        Rectangle rect = new Rectangle(llx, lly, urx, ury);
+        Rectangle rect = new Rectangle(llx, lly, urx-llx, ury-lly);
         PdfCanvas canvas = new PdfCanvas(pdfDoc.addNewPage());
         canvas.setStrokeColor(Color.RED).setLineWidth(0.5f).rectangle(rect).stroke();
         Paragraph p = new Paragraph("This text is centered vertically. " +
                 "It is rendered in the middle of the red rectangle.");
-        p.setFixedLeading(12f);
-        // TODO Since we don't know paragraphs' positions before rendering, we can't center text (feels the absence of SimpleColumn)
-        // rect = new Rectangle(llx, lly, urx-llx, ury - (lly / 2));
-        drawColumnText(pdfDoc, rect, p);
-
+        new Canvas(canvas, pdfDoc, rect).add(p.setFixedPosition(llx, (ury + lly) / 2, urx-llx));
         doc.close();
-    }
-
-    public void drawColumnText(PdfDocument pdfDoc, Rectangle rect, Paragraph p) {
-        new Document(pdfDoc).add(p.setWidth(rect.getWidth()).setHeight(rect.getHeight()).setMarginLeft(rect.getLeft()));
     }
 }
