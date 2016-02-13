bfbbaf83614018962fd7b13ba71f4c569302c65b
Vladimir Osipchuk <vladimir.osipchuk@duallab.com>
2016-02-13 14:03:42 +0300

    Fix minor bug in PdfNode. Revise some samples

--- src/test/java/com/itextpdf/samples/sandbox/tables/CustomBorder4.java
+++
@@ -1,8 +0,0 @@
-/*
-
-    This file is part of the iText (R) project.
-    Copyright (c) 1998-2016 iText Group NV
-
-*/
-
-// TODO No sence in the sample because setting next renderer causes clears the last setted renderer
