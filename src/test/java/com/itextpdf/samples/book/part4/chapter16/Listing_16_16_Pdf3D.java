6ae67864d3de561577039c33171e4939183f2267
pavel.morozov <pavel.morozov@duallab.com>
2016-03-01 16:27:57 +0300

    Implement 3D annotations
    DEVSIX-477

--- src/test/java/com/itextpdf/samples/book/part4/chapter16/Listing_16_16_Pdf3D.java
+++ src/test/java/com/itextpdf/samples/book/part4/chapter16/Listing_16_16_Pdf3D.java
@@ -8,15 +8,10 @@
 package com.itextpdf.samples.book.part4.chapter16;
 
 import com.itextpdf.kernel.geom.Rectangle;
-import com.itextpdf.kernel.pdf.PdfArray;
-import com.itextpdf.kernel.pdf.PdfDictionary;
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfName;
-import com.itextpdf.kernel.pdf.PdfNumber;
-import com.itextpdf.kernel.pdf.PdfOutputStream;
-import com.itextpdf.kernel.pdf.PdfStream;
-import com.itextpdf.kernel.pdf.PdfString;
-import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.*;
+import com.itextpdf.kernel.pdf.annot.Pdf3DAnnotation;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.kernel.pdf.xobject.PdfFormXObject;
 import com.itextpdf.test.annotations.type.SampleTest;
 import com.itextpdf.layout.Document;
 import com.itextpdf.samples.GenericTest;
@@ -26,7 +21,6 @@ import java.io.FileInputStream;
 import org.junit.Ignore;
 import org.junit.experimental.categories.Category;
 
-@Ignore
 @Category(SampleTest.class)
 public class Listing_16_16_Pdf3D extends GenericTest {
     public static final String DEST = "./target/test/resources/book/part4/chapter16/Listing_16_16_Pdf3D.pdf";
@@ -38,13 +32,9 @@ public class Listing_16_16_Pdf3D extends GenericTest {
 
     public void manipulatePdf(String dest) throws Exception {
         PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
-        pdfDoc.addNewPage();
+
         Document doc = new Document(pdfDoc);
-        Rectangle rect = new Rectangle(100, 400, 500, 800);
-//        rect.setBorder(Rectangle.BOX);
-//        rect.setBorderWidth(0.5f);
-//        rect.setBorderColor(new BaseColor(0xFF, 0x00, 0x00));
-//        doc.add(rect);
+        Rectangle rect = new Rectangle(100, 400, 400, 400);
 
         PdfStream stream3D = new PdfStream(pdfDoc, new FileInputStream(RESOURCE));
         stream3D.put(PdfName.Type, new PdfName("3D"));
@@ -61,18 +51,10 @@ public class Listing_16_16_Pdf3D extends GenericTest {
                 new PdfArray(new float[]{1, 0, 0, 0, 0, -1, 0, 1, 0, 3, -235, 28}));
         dict3D.put(PdfName.CO, new PdfNumber(235));
 
-        //TODO 3D Annotation
-        //PdfAnnotation annot = new PdfAnnotation(pdfDoc, rect);
-        // annot.put(PdfName.Contents, new PdfString("3D Model"));
-        // annot.put(PdfName.Subtype, new PdfName("3D"));
-        // annot.put(PdfName.Type, PdfName.Annot);
-//        annot.put(new PdfName("3DD"), stream3D);
-//        annot.put(new PdfName("3DV"), dict3D);
-//        PdfAppearance ap = writer.getDirectContent().createAppearance(rect.getWidth(), rect.getHeight());
-//        annot.setAppearance(PdfAnnotation.APPEARANCE_NORMAL, ap);
-//        annot.setPage();
-
-//        pdfDoc.addNewPage().addAnnotation(annot);
+        Pdf3DAnnotation annot = new Pdf3DAnnotation(rect, stream3D);
+        annot.setContents(new PdfString("3D Model"));
+        annot.setDefaultInitialView(dict3D);
+        pdfDoc.addNewPage().addAnnotation(annot);
         doc.close();
     }
 }
