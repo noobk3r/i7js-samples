c48bb9dea03fdc8df6a523b76fef4c2013d56068
Alexander Chingarev <alexander.chingarev@gmail.com>
2016-03-18 17:36:55 +0300

    Added chapter 7 examples

---
+++ src/main/java/tutorial/chapter07/C07E01_QuickBrownFox_PDFUA.java
@@ -0,0 +1,80 @@
+/*
+ * PDF/UA example
+ */
+package tutorial.chapter07;
+
+import com.itextpdf.io.font.PdfEncodings;
+import com.itextpdf.io.image.ImageFactory;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.pdf.*;
+import com.itextpdf.kernel.xmp.XMPException;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Image;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Text;
+
+import java.io.File;
+import java.io.IOException;
+
+public class C07E01_QuickBrownFox_PDFUA {
+    public static final String DOG = "src/main/resources/img/dog.bmp";
+    public static final String FOX = "src/main/resources/img/fox.bmp";
+    public static final String FONT = "src/main/resources/font/FreeSans.ttf";
+        
+    public static final String DEST = "results/chapter07/quick_brown_fox_PDFUA.pdf";
+    
+    public static void main(String args[]) throws IOException, XMPException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new C07E01_QuickBrownFox_PDFUA().createPdf(DEST);
+    }
+    
+    public void createPdf(String dest) throws IOException, XMPException {
+        //Initialize PDFA document with output intent
+        PdfDocument pdf = new PdfDocument(new PdfWriter(dest));
+
+        //Initialize document
+        Document document = new Document(pdf);
+
+        //Make document tagged
+        pdf.setTagged();
+
+        pdf.getCatalog().setViewerPreferences(new PdfViewerPreferences().setDisplayDocTitle(true));
+        pdf.getCatalog().setLang(new PdfString("en-US"));
+        PdfDocumentInfo info = pdf.getDocumentInfo();
+        info.setTitle("iText7 PDF/UA example");
+
+        //Create XMP meta data
+        pdf.setXmpMetadata();
+
+        Paragraph p = new Paragraph();
+
+        //Embed font
+        PdfFont font = PdfFontFactory.createFont(FONT, PdfEncodings.WINANSI, true);
+        p.setFont(font);
+
+        p.add(new Text("The quick brown "));
+
+        Image foxImage = new Image(ImageFactory.getImage(FOX));
+
+        //PDF/UA
+        //Set alt text
+        foxImage.getAccessibilityProperties().setAlternateDescription("Fox");
+
+        p.add(foxImage);
+
+        p.add(" jumps over the lazy ");
+
+        Image dogImage = new Image(ImageFactory.getImage(DOG));
+
+        //PDF/UA
+        //Set alt text
+        dogImage.getAccessibilityProperties().setAlternateDescription("Dog");
+
+        p.add(dogImage);
+
+        document.add(p);
+        document.close();
+    }
+}
---
+++ src/main/java/tutorial/chapter07/C07E03_UnitedStates_PDFA_3a.java
@@ -0,0 +1,99 @@
+/*
+ * PDF/A-3a example
+ */
+package tutorial.chapter07;
+
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.pdf.*;
+import com.itextpdf.kernel.pdf.filespec.PdfFileSpec;
+import com.itextpdf.kernel.xmp.XMPException;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.Property;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.pdfa.PdfADocument;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.StringTokenizer;
+
+public class C07E03_UnitedStates_PDFA_3a {
+    public static final String DATA = "src/main/resources/data/united_states.csv";
+    public static final String FONT = "src/main/resources/font/FreeSans.ttf";
+    public static final String BOLD_FONT = "src/main/resources/font/FreeSansBold.ttf";
+    public static final String INTENT = "src/main/resources/color/sRGB_CS_profile.icm";
+
+
+    public static final String DEST = "results/chapter07/united_states_PDFA-3a.pdf";
+    
+    public static void main(String args[]) throws IOException, XMPException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new C07E03_UnitedStates_PDFA_3a().createPdf(DEST);
+    }
+    
+    public void createPdf(String dest) throws IOException, XMPException {
+        //Initialize PDFA document with output intent
+        PdfADocument pdf = new PdfADocument(new PdfWriter(dest), PdfAConformanceLevel.PDF_A_3A,
+                new PdfOutputIntent("Custom", "", "http://www.color.org", "sRGB IEC61966-2.1", new FileInputStream(INTENT)));
+
+        //Initialize document
+        Document document = new Document(pdf, PageSize.A4.rotate());
+
+        //Make document tagged
+        pdf.setTagged();
+
+        //Set document metadata
+        pdf.getCatalog().setViewerPreferences(new PdfViewerPreferences().setDisplayDocTitle(true));
+        pdf.getCatalog().setLang(new PdfString("en-US"));
+        PdfDocumentInfo info = pdf.getDocumentInfo();
+        info.setTitle("iText7 PDF/A-3 example");
+
+        //Create XMP meta data
+        pdf.setXmpMetadata();
+
+        //Add attachment
+        PdfDictionary parameters = new PdfDictionary();
+        parameters.put(PdfName.ModDate, new PdfDate().getPdfObject());
+        PdfFileSpec fileSpec = PdfFileSpec.createEmbeddedFileSpec(pdf, Files.readAllBytes(Paths.get(DATA)), "united_states.csv",
+                "united_states.csv", new PdfName("text/csv"), parameters, PdfName.Data, false);
+        fileSpec.put(new PdfName("AFRelationship"), new PdfName("Data"));
+        pdf.addFileAttachment("united_states.csv", fileSpec);
+        PdfArray array = new PdfArray();
+        array.add(fileSpec.getPdfObject().getIndirectReference());
+        pdf.getCatalog().put(new PdfName("AF"), array);
+
+        //Embed fonts
+        PdfFont font = PdfFontFactory.createFont(FONT, true);
+        PdfFont bold = PdfFontFactory.createFont(BOLD_FONT, true);
+
+        Table table = new Table(new float[]{4, 1, 3, 4, 3, 3, 3, 3, 1});
+        table.setWidthPercent(100);
+        BufferedReader br = new BufferedReader(new FileReader(DATA));
+        String line = br.readLine();
+        process(table, line, bold, true);
+        while ((line = br.readLine()) != null) {
+            process(table, line, font, false);
+        }
+        br.close();
+        document.add(table);
+        
+        //Close document
+        document.close();
+    }
+
+    public void process(Table table, String line, PdfFont font, boolean isHeader) {
+        StringTokenizer tokenizer = new StringTokenizer(line, ";");
+        while (tokenizer.hasMoreTokens()) {
+            if (isHeader) {
+                table.addHeaderCell(new Cell().setHorizontalAlignment(Property.HorizontalAlignment.CENTER).add(new Paragraph(tokenizer.nextToken()).setHorizontalAlignment(Property.HorizontalAlignment.CENTER).setFont(font)));
+            } else {
+                table.addCell(new Cell().setHorizontalAlignment(Property.HorizontalAlignment.CENTER).add(new Paragraph(tokenizer.nextToken()).setHorizontalAlignment(Property.HorizontalAlignment.CENTER).setFont(font)));
+            }
+        }
+    }
+}
