8ebfeee37d38d520876a7fda78b5066e8dcf77dc
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2016-04-07 11:54:39 +0200

    Cosmetic changes to the Axes example

--- src/main/java/tutorial/chapter02/C02E01_Axes.java
+++ src/main/java/tutorial/chapter02/C02E01_Axes.java
@@ -8,6 +8,7 @@ import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfPage;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvasConstants;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -57,30 +58,33 @@ public class C02E01_Axes {
                 .stroke();
 
         //Draw X axis arrow
-        canvas.setLineJoinStyle(1).moveTo(ps.getWidth() / 2 - 25, -10)
+        canvas.setLineJoinStyle(PdfCanvasConstants.LineJoinStyle.ROUND)
+                .moveTo(ps.getWidth() / 2 - 25, -10)
                 .lineTo(ps.getWidth() / 2 - 15, 0)
                 .lineTo(ps.getWidth() / 2 - 25, 10).stroke()
-                .setLineJoinStyle(0);
+                .setLineJoinStyle(PdfCanvasConstants.LineJoinStyle.MITER);
 
         //Draw Y axis
         canvas.moveTo(0, -(ps.getHeight() / 2 - 15))
                 .lineTo(0, ps.getHeight() / 2 - 15)
                 .stroke();
+        
         //Draw Y axis arrow
-        canvas.setLineJoinStyle(1).moveTo(-10, ps.getHeight() / 2 - 25)
+        canvas.saveState()
+                .setLineJoinStyle(PdfCanvasConstants.LineJoinStyle.ROUND)
+                .moveTo(-10, ps.getHeight() / 2 - 25)
                 .lineTo(0, ps.getHeight() / 2 - 15)
                 .lineTo(10, ps.getHeight() / 2 - 25).stroke()
-                .setLineJoinStyle(0);
+                .restoreState();
 
         //Draw X serif
         for (int i = -((int) ps.getWidth() / 2 - 61); i < ((int) ps.getWidth() / 2 - 60); i += 40) {
-            canvas.moveTo(i, 5)
-                    .lineTo(i, -5).stroke();
+            canvas.moveTo(i, 5).lineTo(i, -5);
         }
         //Draw Y serif
         for (int j = -((int) ps.getHeight() / 2 - 57); j < ((int) ps.getHeight() / 2 - 56); j += 40) {
-            canvas.moveTo(5, j)
-                    .lineTo(-5, j).stroke();
+            canvas.moveTo(5, j).lineTo(-5, j);
         }
+        canvas.stroke();
     }
 }
