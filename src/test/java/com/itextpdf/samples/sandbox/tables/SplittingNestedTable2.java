152e9a3200895699ac311d5f8fe76be4ea0723d8
Vladimir Osipchuk <vladimir.osipchuk@duallab.com>
2016-05-17 15:31:02 +0300

    Fix wrong splitting. Fix wrong area updating
    Shift cells in columns up if necessary to split table corectly
    Update are first with nextStoredArea, and only then using updateCurrentArea method

--- src/test/java/com/itextpdf/samples/sandbox/tables/SplittingNestedTable2.java
+++ src/test/java/com/itextpdf/samples/sandbox/tables/SplittingNestedTable2.java
@@ -15,19 +15,18 @@ import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
-import com.itextpdf.layout.property.HorizontalAlignment;
 import com.itextpdf.layout.element.AreaBreak;
 import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.property.HorizontalAlignment;
 import com.itextpdf.layout.property.VerticalAlignment;
 import com.itextpdf.samples.GenericTest;
 import com.itextpdf.test.annotations.type.SampleTest;
-
-import java.io.File;
-
 import org.junit.Ignore;
 import org.junit.experimental.categories.Category;
+
+import java.io.File;
 
 @Ignore
 @Category(SampleTest.class)
@@ -44,7 +43,7 @@ public class SplittingNestedTable2 extends GenericTest {
     protected void manipulatePdf(String dest) throws Exception {
         PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
         // TODO DEVSIX-466
-        Document doc = new Document(pdfDoc, new PageSize(300, 150));
+        Document doc = new Document(pdfDoc, new PageSize(300, 120));
         doc.add(new Paragraph("Table with setKeepTogether(false):"));
         Table table = new Table(2);
         table.setMarginTop(10);
@@ -62,11 +61,29 @@ public class SplittingNestedTable2 extends GenericTest {
         cell = new Cell().add(inner);
         cell.setPadding(0);
         table.addCell(cell);
+
         doc.add(table);
         doc.add(new AreaBreak());
 
         doc.add(new Paragraph("Table with setKeepTogether(true):"));
+        table = new Table(2);
         table.setKeepTogether(true);
+        table.setMarginTop(10);
+        cell = new Cell().add("GROUPS");
+        cell.setRotationAngle(Math.toRadians(90));
+        cell.setVerticalAlignment(VerticalAlignment.MIDDLE);
+        cell.setHorizontalAlignment(HorizontalAlignment.CENTER);
+        table.addCell(cell);
+        inner = new Table(1);
+        inner.addCell("row 1");
+        inner.addCell("row 2");
+        inner.addCell("row 3");
+        inner.addCell("row 4");
+        inner.addCell("row 5");
+        cell = new Cell().add(inner);
+        cell.setPadding(0);
+        table.addCell(cell);
+
         doc.add(table);
 
         doc.close();
