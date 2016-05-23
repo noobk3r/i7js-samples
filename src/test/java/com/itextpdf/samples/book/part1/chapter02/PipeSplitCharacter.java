eaea860a85f4fbddb0ad20aaca20f57ff61ca9cc
Pavel Alay <pavel.alay@duallab.com>
2016-05-23 20:35:11 +0300

    Minor changes with Glyph.getUnicode().

--- src/test/java/com/itextpdf/samples/book/part1/chapter02/PipeSplitCharacter.java
+++ src/test/java/com/itextpdf/samples/book/part1/chapter02/PipeSplitCharacter.java
@@ -14,7 +14,7 @@ public class PipeSplitCharacter implements ISplitCharacters {
 
     @Override
     public boolean isSplitCharacter(GlyphLine text, int glyphPos) {
-        if (text.get(glyphPos).getUnicode() == null) {
+        if (!text.get(glyphPos).hasValidUnicode()) {
             return false;
         }
         int charCode = text.get(glyphPos).getUnicode();
