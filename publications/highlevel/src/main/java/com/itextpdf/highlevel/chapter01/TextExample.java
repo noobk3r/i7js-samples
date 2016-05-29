e816bcf59813d59d28e14e85ca44f10c3447a18c
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2016-05-29 11:59:48 +0200

    new examples for the layout tutorial

--- publications/highlevel/src/main/java/com/itextpdf/highlevel/chapter01/TextExample.java
+++ publications/highlevel/src/main/java/com/itextpdf/highlevel/chapter01/TextExample.java
@@ -4,9 +4,14 @@
  */
 package com.itextpdf.highlevel.chapter01;
 
+import com.itextpdf.io.font.FontConstants;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Text;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -16,7 +21,7 @@ import java.io.IOException;
  */
 public class TextExample {
     
-    public static final String DEST = "results/chapter01/jekyll_hyde_v1.pdf";
+    public static final String DEST = "results/chapter01/text_example.pdf";
     
     public static void main(String args[]) throws IOException {
         File file = new File(DEST);
@@ -35,7 +40,13 @@ public class TextExample {
         // Initialize document
         Document document = new Document(pdf);
         
-        Text title = new Text("D");
+        
+        PdfFont font = PdfFontFactory.createFont(FontConstants.TIMES_ROMAN);
+        PdfFont bold = PdfFontFactory.createFont(FontConstants.TIMES_BOLD);
+        Text title = new Text("The Strange Case of Dr. Jekyll and Mr. Hyde").setFont(bold);
+        Text author = new Text("Robert Louis Stevenson").setFont(font);
+        Paragraph p = new Paragraph().add(title).add(" by ").add(author);
+        document.add(p);
         
         //Close document
         document.close();
