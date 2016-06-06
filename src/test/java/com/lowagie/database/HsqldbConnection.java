e47397fcecfd6d28866466f8ad13f277449f5cde
Amedee Van Gasse <amedee.vangasse@itextpdf.com>
2016-06-06 13:41:36 +0200

    Don't shut down the database explictly
    Tear down the database declaratively by adding `shutdown=true` to the initial connection
    
    See http://stackoverflow.com/a/3257047/766786

--- src/test/java/com/lowagie/database/HsqldbConnection.java
+++ src/test/java/com/lowagie/database/HsqldbConnection.java
@@ -9,7 +9,6 @@ package com.lowagie.database;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
  * This is a helper class to access an hsqldb database.
@@ -32,15 +31,7 @@ public class HsqldbConnection extends DatabaseConnection {
             throw new SQLException("HSQLDB database driver not found");
         }
         connection = DriverManager.getConnection(
-            "jdbc:hsqldb:src/test/resources/db/" + db_file_name_prefix, "SA", "");
+            "jdbc:hsqldb:src/test/resources/db/" + db_file_name_prefix + ";shutdown=true", "SA", "");
     }
-    
-    /**
-     * Shuts down the database and closes the connection.
-     */
-    public void close() throws SQLException {
-        Statement st = createStatement();
-        st.execute("SHUTDOWN");
-        super.close();
-    }
+
 }
